b299d728cdcdd50af194076a0335a34cd8f08d31
yuanbaoyaoyao <583515921@qq.com>
2022-04-26 09:45:36 +0800

    first commit

---
+++ entry/src/main/java/com/example/record_harmonyos/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.record_harmonyos;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/ohosTest/java/com/example/record_harmonyos/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.example.record_harmonyos;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("com.example.record_harmonyos", actualBundleName);
+    }
+}
